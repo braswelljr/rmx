75c414689bfa791cbdba3dced5c71e4be1679a54
braswelljr <braswellkenneth7@gmail.com>
2022-12-14 15:40:32 +0000

    Modify the Run function

--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-  "os"
+	"os"
 
-  "github.com/braswelljr/rmx/rm"
+	"github.com/braswelljr/rmx/rm"
 
-  "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-  // get the root command
-  rmr := rm.RM{
-    Stdin:  os.Stdin,
-    Stdout: os.Stdout,
-  }
+	// get the root command
+	rmr := rm.RM{
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+	}
 
-  // initialize cobra
-  command := &cobra.Command{
-    Use:   "rmx [OPTION]... [FILE]...",
-    Short: "A cross-platform replacement for UNIX rm command",
-    Long:  "A cross-platform replacement for UNIX rm command",
-    RunE: func(command *cobra.Command, args []string) error {
-      rmr.Directory = "."
-      if len(args) > 0 {
-        rmr.Directory = args[0]
+	// initialize cobra
+	command := &cobra.Command{
+		Use:   "rmx [OPTION]... [FILE]...",
+		Short: "A cross-platform replacement for UNIX rm command",
+		Long:  "A cross-platform replacement for UNIX rm command",
+		RunE: func(command *cobra.Command, args []string) error {
+			rmr.Directory = "."
+			if len(args) > 0 {
+				rmr.Directory = args[0]
 
-        // run command and return any error
-        return rmr.Run(args)
-      }
+				// run command and return any error
+				return rmr.Run(command, args)
+			}
 
-      return rmr.ShowHelp()
-    },
-  }
+			return rmr.ShowHelp()
+		},
+	}
 
-  // add flags
-  command.PersistentFlags().BoolP("help", "h", false, "help for this command")
-  command.Flags().BoolVarP(&rmr.Flags.F, "force", "f", false, "ignore nonexistent files and arguments, never prompt")
-  command.Flags().BoolVarP(&rmr.Flags.Ii, "interactive", "i", false, "prompt before every removal")
-  //command.Flags().BoolVarP(&rmr.Flags.II, "interactive", "I", false, "prompt once before removing more than three files, or when removing recursively; less intrusive than -i, while still giving protection against most mistakes")
-  command.Flags().BoolVarP(&rmr.Flags.Rr, "recursive", "r", false, "remove directories and their contents recursively")
-  //command.Flags().BoolVarP(&rmr.Flags.RR, "recursive", "R", false, "remove directories and their contents recursively")
-  command.Flags().BoolVarP(&rmr.Flags.D, "dir", "d", false, "remove empty directories")
-  command.Flags().BoolVarP(&rmr.Flags.V, "verbose", "v", false, "explain what is being done")
+	// add flags
+	command.PersistentFlags().BoolP("help", "h", false, "help for this command")
+	command.Flags().BoolVarP(&rmr.Flags.F, "force", "f", false, "ignore nonexistent files and arguments, never prompt")
+	command.Flags().BoolVarP(&rmr.Flags.Ii, "interactive", "i", false, "prompt before every removal")
+	//command.Flags().BoolVarP(&rmr.Flags.II, "interactive", "I", false, "prompt once before removing more than three files, or when removing recursively; less intrusive than -i, while still giving protection against most mistakes")
+	command.Flags().BoolVarP(&rmr.Flags.Rr, "recursive", "r", false, "remove directories and their contents recursively")
+	//command.Flags().BoolVarP(&rmr.Flags.RR, "recursive", "R", false, "remove directories and their contents recursively")
+	command.Flags().BoolVarP(&rmr.Flags.D, "dir", "d", false, "remove empty directories")
+	command.Flags().BoolVarP(&rmr.Flags.V, "verbose", "v", false, "explain what is being done")
 
-  // clean up and exit on error
-  if err := command.Execute(); err != nil {
-    os.Exit(1)
-  }
+	// clean up and exit on error
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
--- rm/rm.go
+++ rm/rm.go
@@ -1,93 +1,34 @@
 package rm
 
 import (
-  "github.com/braswelljr/rmx/pkg/utils"
-  "os"
+	"github.com/spf13/cobra"
 )
 
 // ShowHelp - show the help message
 func (rm *RM) ShowHelp() error {
-  // show the help message
-  println("rmx - remove files or directories")
-  println("Usage: rmx [OPTION]... [FILE]...")
-  println("Remove (unlink) the FILE(s).")
-  println("  -f, --force           ignore nonexistent files and arguments, never prompt")
-  println("  -i                    prompt before every removal")
-  println("  -I                    prompt once before removing more than three files, or when removing recursively; less intrusive than -i, while still giving protection against most mistakes")
-  println("  -r, -R                remove directories and their contents recursively")
-  println("  -d                    remove empty directories")
-  println("  -v                    explain what is being done")
-  println("      --help     display this help and exit")
-  println("      --version  output version information and exit")
-  println("Note:  rm does not remove directories.  Use rmdir to remove directories.")
-  return nil
+	// show the help message
+	println("rmx - remove files or directories")
+	println("Usage: rmx [OPTION]... [FILE]...")
+	println("Remove (unlink) the FILE(s).")
+	println("  -f, --force           ignore nonexistent files and arguments, never prompt")
+	println("  -i                    prompt before every removal")
+	println("  -I                    prompt once before removing more than three files, or when removing recursively; less intrusive than -i, while still giving protection against most mistakes")
+	println("  -r, -R                remove directories and their contents recursively")
+	println("  -d                    remove empty directories")
+	println("  -v                    explain what is being done")
+	println("      --help     display this help and exit")
+	println("      --version  output version information and exit")
+	println("Note:  rm does not remove directories.  Use rmdir to remove directories.")
+
+	return nil
 }
 
-func (rm *RM) Run(args []string) error {
-  // check for empty args
-  if len(args) < 1 {
-    return rm.ShowHelp()
-  }
+func (rm *RM) Run(command *cobra.Command, args []string) error {
+	// check for empty args or help flag
+	if command.Flags().NFlag() < 1 || command.Flags().Changed("help") {
+		return rm.ShowHelp()
+	}
 
-  // get the directory
-  dir, err := os.Open(rm.Directory)
-  if err != nil {
-    panic(err)
-  }
-
-  // get the directory info
-  dirInfo, err := dir.Stat()
-  if err != nil {
-    panic(err)
-  }
-
-  // check if the directory is a directory
-  if dirInfo.IsDir() {
-    // get the directory entries
-    dirEntries, err := dir.Readdir(0)
-    if err != nil {
-      panic(err)
-    }
-
-    var Directories Directories
-
-    // loop through the directory entries
-    for _, dirEntry := range dirEntries {
-      // check if the directory entry is a directory
-      if dirEntry.IsDir() {
-        // get the directory path
-        dirPath := rm.Directory + string(os.PathSeparator) + dirEntry.Name()
-
-        // create a new directory
-        directory := Directory{
-          Name: dirEntry.Name(),
-          Path: dirPath,
-          Info: dirEntry,
-        }
-
-        // add the directory to the directories
-        Directories = append(Directories, directory)
-      }
-    }
-
-    // loop through the directories
-    for _, directory := range Directories {
-      // check if the directory is empty
-      if utils.IsEmpty(directory.Path) {
-        // remove the directory
-        if err := os.Remove(directory.Path); err != nil {
-          panic(err)
-        }
-
-        // check if the verbose flag is set
-        if rm.Flags.V {
-          // print the directory
-          println(directory.Path)
-        }
-      }
-    }
-  }
-
-  // close the directory
-  return nil
+	// close the directory
+	return nil
 }
