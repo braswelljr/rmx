fa2c2c76e04c591d66222d84cf332d28168dd75b
braswelljr <braswellkenneth7@gmail.com>
2022-12-15 11:43:46 +0000

    Update Util Tests

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
@@ -11,65 +10,73 @@ import (
 
 // TestIsHidden - check if a file is hidden
 func TestIsHidden(t *testing.T) {
+	// create a temporary directory to test depending on the OS
+	dir := os.TempDir()
+	var filesAndDirs []string
+
+	// read the directory and get all files/directories
+	if err := filepath.Walk(dir, func(_ string, info os.FileInfo, _ error) error {
+		// append the file/directory name to the slice
+		filesAndDirs = append(filesAndDirs, info.Name())
+		return nil
+	}); err != nil {
+		t.Error("Error reading directory", err)
+	}
+
+	// if there are no files/directories, create a hidden file and check if it is hidden
+	if len(filesAndDirs) < 1 {
+		// create two files, one hidden and one not hidden
+		files := []string{"/test.txt", "/.test.txt"}
+		// loop through the files and create them
+		for _, file := range files {
+			if _, err := os.Create(dir + file); err != nil {
+				t.Error("Creating file: ", err)
+			}
+		}
+	}
+
+	// if there are files/directories, check if they are hidden
 	t.Run("IsHidden", func(t *testing.T) {
-		// create a temporary directory to test depending on the OS
-		dir, err := ioutil.TempDir("", "test")
-		if err != nil {
-			t.Error("Creating directory: ", err)
+		// check if the file is hidden
+		if IsHidden(filesAndDirs[0], false) {
+			assert.Equal(t, true, IsHidden(filesAndDirs[0], false), "File should be hidden")
 		}
-
-		var filesAndDirs []string
-
-		// read the directory and get all files/directories
-		if err := filepath.Walk(dir, func(_ string, info os.FileInfo, _ error) error {
-			// append the file/directory name to the slice
-			filesAndDirs = append(filesAndDirs, info.Name())
-			return nil
-		}); err != nil {
-			t.Error("Error reading directory", err)
-		}
-
-		// if there are no files/directories, create a hidden file and check if it is hidden
-		if len(filesAndDirs) < 1 {
-			// create two files, one hidden and one not hidden
-			if err := ioutil.WriteFile(dir+"/test.txt", []byte("test"), 0644); err != nil {
-				t.Error("Creating file: ", err)
-			}
-			if err := ioutil.WriteFile(dir+"/.test.txt", []byte("test"), 0644); err != nil {
-				t.Error("Creating file: ", err)
-			}
-		}
-
-		// if there are files/directories, check if they are hidden
-		for _, file := range filesAndDirs {
-			if IsHidden(file, false) {
-				t.Error("File should not be hidden")
-			}
-		}
-
 	})
+
+	t.Run("IsNotHidden", func(t *testing.T) {
+		// check if the file is not hidden
+		if !IsHidden(filesAndDirs[1], false) {
+			assert.Equal(t, false, IsHidden(filesAndDirs[1], false), "File should not be hidden")
+		}
+	})
+
 }
 
 // TestIsEmpty - check if a directory is empty
 func TestIsEmpty(t *testing.T) {
+	// isEmpty
 	t.Run("IsEmpty", func(t *testing.T) {
 		// create a temporary directory to test depending on the OS
-		dir, err := ioutil.TempDir("", "test")
-		if err != nil {
-			t.Error("Creating directory: ", err)
+		dir := os.TempDir()
+
+		// check if the directory is empty
+		if IsEmpty(dir) {
+			assert.Equal(t, true, IsEmpty(dir), "Directory should be empty")
+		}
+	})
+
+	// isNotEmpty
+	t.Run("IsNotEmpty", func(t *testing.T) {
+		// create a temporary directory to test depending on the OS
+		dir := os.TempDir()
+
+		// create a file in the directory
+		if _, err := os.Create(dir + "/test.txt"); err != nil {
+			t.Error("Creating file: ", err)
 		}
 
 		// check if the directory is empty
 		if !IsEmpty(dir) {
-			assert.Equal(t, true, IsEmpty(dir), "Directory should be empty")
-		}
-
-		// create a file in the directory and check if it is empty
-		if err := ioutil.WriteFile(dir+"/test.txt", []byte("test"), 0644); err != nil {
-			t.Error("Creating file: ", err)
-		}
-
-		if IsEmpty(dir) {
 			assert.Equal(t, false, IsEmpty(dir), "Directory should not be empty")
 		}
 	})
